3d0b0d9925780acc8a185bf77acdab23ec90a639
Alexander Shikhalev <kiskamiska2000@gmail.com>
2019-06-17 05:56:37 +0300

    Functions header file add

---
+++ functions.h
@@ -0,0 +1,11 @@
+#ifndef CURSTRPO_FUNCTIONS_H
+#define CURSTRPO_FUNCTIONS_H
+
+int notepadpw();
+int weather();
+int ipcheck();
+int browser();
+int fun();
+char* concat(const char *s1, const char *s2);
+int main();
+#endif
